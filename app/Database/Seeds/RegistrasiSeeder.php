54c270d07e3d45c47157167a1fa626a30dddc0db
Kachirikilu <muttaqien.wildan12@gmail.com>
2025-09-09 16:31:02 +0700

    Seeder

---
+++ app/Database/Seeds/RegistrasiSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class RegistrasiSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'Name' => 'Wildan Athif Muttaqien',
+            'Email' => 'muttaqien.wildan12@gmail.com',
+            'UserName' => 'wildan12',
+            'Password' => '$2y$10$rOtdcYqn6KARyq1E9tSty.lEL.eV1i8D9O57afAuO4y/jvw8zzeMi',
+            'WhatsApp' => '-',
+            'Instagram' => '-',
+            'Facebook' => '-',
+            'Profil' => '1757408535_ef31e0bae32ec2f3d716.jpg',
+            'Pangkat' => 'CEO',
+            'Color' => 'nightEdition',
+            'Image' => 'W2',
+            'View' => 0,
+        ];
+
+        $this->db->table('registrasi')->insert($data);
+    }
+}
